9da72a544e4f2914b0951e2a0369fea369da720f
Pawel Koziol <pp.koziol@gmail.com>
2014-04-29 00:18:36 +0200

    Update trans.h

--- trans.h
+++ trans.h
@@ -17,33 +17,147 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#pragma once
+#include <string.h>
+#include <stdio.h>
+#if defined(_WIN32) || defined(_WIN64)
+#  include <windows.h>
+#else
+#  include <unistd.h>
+#  include <sys/time.h>
+#endif
+#include "data.h"
+#include "rodent.h"
 
-typedef struct {      // transposition table entry
-  U64 key;
-  short date;
-  short move;
-  short score;
-  unsigned char flags;
-  unsigned char depth;
-} ENTRY;
+int InputAvailable(void)
+{
+#if defined(_WIN32) || defined(_WIN64)
+  static int init = 0, pipe;
+  static HANDLE inh;
+  DWORD dw;
 
-struct sTransTable {  // transposition table with access functions
-private:
-  int tt_size;
-  int tt_mask;
-  int tt_date;
-  ENTRY *tt;
-public:
-  U64 InitHashKey(sPosition *p);
-  U64 InitPawnKey(sPosition *p);
-  void Alloc(int);
-  void Clear(void);
-  int Retrieve(U64 key, int *move, int *score, int alpha, int beta, int depth, int ply);
-  void RetrieveMove(U64 key, int *move );
-  int RefineScore(U64 key, int score);
-  void Store(U64 key, int move, int score, int flags, int depth, int ply);
-  void ChangeDate();
-};
+  if (!init) {
+    init = 1;
+    inh = GetStdHandle(STD_INPUT_HANDLE);
+    pipe = !GetConsoleMode(inh, &dw);
+    if (!pipe) {
+      SetConsoleMode(inh, dw & ~(ENABLE_MOUSE_INPUT | ENABLE_WINDOW_INPUT));
+      FlushConsoleInputBuffer(inh);
+    }
+  }
+  if (pipe) {
+    if (!PeekNamedPipe(inh, NULL, 0, NULL, &dw, NULL))
+      return 1;
+    return dw > 0;
+  } else {
+    GetNumberOfConsoleInputEvents(inh, &dw);
+    return dw > 1;
+  }
+#else
+  fd_set readfds;
+  struct timeval tv;
 
-extern sTransTable TransTable;
+  FD_ZERO(&readfds);
+  FD_SET(STDIN_FILENO, &readfds);
+  tv.tv_sec = 0;
+  tv.tv_usec = 0;
+  select(STDIN_FILENO + 1, &readfds, NULL, NULL, &tv);
+  return FD_ISSET(STDIN_FILENO, &readfds);
+#endif
+}
+
+U64 Random64(void)
+{
+  static U64 next = 1;
+
+  next = next * 1103515245 + 12345;
+  return next;
+}
+
+void MoveToStr(int move, char *moveString)
+{
+  static const char prom_char[5] = "nbrq";
+
+  moveString[0] = File(Fsq(move)) + 'a';
+  moveString[1] = Rank(Fsq(move)) + '1';
+  moveString[2] = File(Tsq(move)) + 'a';
+  moveString[3] = Rank(Tsq(move)) + '1';
+  moveString[4] = '\0';
+  if (IsProm(move)) {
+    moveString[4] = prom_char[(move >> 12) & 3];
+    moveString[5] = '\0';
+  }
+}
+
+void PrintMove(int move)
+{
+	 char moveString[6];
+	 MoveToStr(move, moveString);
+	 printf("%s", moveString);
+}
+
+int StrToMove(sPosition *p, char *moveString)
+{
+  int from, to, type;
+
+  from = Sq(moveString[0] - 'a', moveString[1] - '1');
+  to   = Sq(moveString[2] - 'a', moveString[3] - '1');
+  type = NORMAL;
+  if (TpOnSq(p, from) == K && Abs(to - from) == 2)
+    type = CASTLE;
+  else if (TpOnSq(p, from) == P) {
+    if (to == p->epSquare) 
+      type = EP_CAP;
+    else if (Abs(to - from) == 16)
+      type = EP_SET;
+    else if (moveString[4] != '\0')
+      switch (moveString[4]) {
+      case 'n':
+        type = N_PROM;
+        break;
+      case 'b':
+        type = B_PROM;
+        break;
+      case 'r':
+        type = R_PROM;
+        break;
+      case 'q':
+        type = Q_PROM;
+        break;
+      }
+  }
+  return (type << 12) | (to << 6) | from;
+}
+
+void PvToStr(int *pv, char *pv_str)
+{
+  int *movep;
+  char moveString[6];
+
+  pv_str[0] = '\0';
+
+  for (movep = pv; *movep; movep++) {
+    MoveToStr(*movep, moveString);
+    strcat(pv_str, moveString);
+    strcat(pv_str, " ");
+  }
+}
+
+void BuildPv(int *dst, int *src, int move)
+{
+  *dst++ = move;
+  while ((*dst++ = *src++))
+    ;
+}
+
+U64 atoull(const char *s)
+{
+  U64 x = 0;
+  char c;
+  while (c = *s++) {
+	if (c != ' ' && c != ',') {
+       x *= 10;
+       x += c - '0';
+	}
+  }
+  return x;
+} 
