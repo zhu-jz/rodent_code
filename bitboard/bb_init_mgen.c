7c7587aa41d9ebe4458c0c3859b0d581e35cff0f
Pawel Koziol <pp.koziol@gmail.com>
2014-08-24 14:24:42 +0200

    comments and formatting

--- bitboard/bb_init_mgen.c
+++ bitboard/bb_init_mgen.c
@@ -21,54 +21,59 @@
 #include "../rodent.h"
 
 static const int dirs[4][2] = {{1, -1}, {16, -16}, {17, -17}, {15, -15}};
+static const U64 bbDIAG_A1H8 = 0x8040201008040201; // constants shifted to create diagonal bitmasks
+static const U64 bbDIAG_A8H1 = 0x0102040810204080;
+
+// Stuff initialized here is used by the macros RAttacks, BAtacks and QAttacks.
+// To see the use of these tables, study rodent.h, just before aforementioned macros.
 
 void InitKindergartenBitboards()
 {
-  int i, j, k, l, x, y;
+   int i, j, k, l, x, y;
 
-  // constants shifted to create bitmasks
-  const U64 bbDIAG_A1H8 = 0x8040201008040201;
-  const U64 bbDIAG_A8H1 = 0x0102040810204080;
+   // init masks
+   for (i = 0; i < 64; i++) {
 
-  // init line masks
-  for (i = 0; i < 64; i++) {
-    bbLineMask[HOR][i] = bbRANK_1 << (i & 070);
-    bbLineMask[VER][i] = bbFILE_A << (i & 007);
+	  // line masks
+      bbLineMask[HOR][i] = bbRANK_1 << (i & 070);
+      bbLineMask[VER][i] = bbFILE_A << (i & 007);
 
-    j = File(i) - Rank(i);
-    if (j > 0) bbLineMask[DIAG_AH][i] = bbDIAG_A1H8 >> (j * 8);
-    else       bbLineMask[DIAG_AH][i] = bbDIAG_A1H8 << (-j * 8);
+	  // 1st set of diagonal masks
+      j = File(i) - Rank(i);
+      if (j > 0) bbLineMask[DIAG_AH][i] = bbDIAG_A1H8 >> (j * 8);
+      else       bbLineMask[DIAG_AH][i] = bbDIAG_A1H8 << (-j * 8);
 
-    j = File(i) - (RANK_8 - Rank(i));
-    if (j > 0) bbLineMask[DIAG_HA][i] = bbDIAG_A8H1 << (j * 8);
-    else       bbLineMask[DIAG_HA][i] = bbDIAG_A8H1 >> (-j * 8);
-  }
+	  // 2nd set of diagonal masks
+      j = File(i) - (RANK_8 - Rank(i));
+      if (j > 0) bbLineMask[DIAG_HA][i] = bbDIAG_A8H1 << (j * 8);
+      else       bbLineMask[DIAG_HA][i] = bbDIAG_A8H1 >> (-j * 8);
+   }
 
-  // init sliding piece attacks
-  for (i = 0; i < 4; i++)
-    for (j = 0; j < 64; j++)
-      for (k = 0; k < 64; k++) {
-        attacks[i][j][k] = 0;
+   // init sliding piece attacks
+   for (i = 0; i < 4; i++)
+      for (j = 0; j < 64; j++)
+         for (k = 0; k < 64; k++) {
+            attacks[i][j][k] = 0;
 
-          for (l = 0; l < 2; l++) {
-          x = Map0x88(j) + dirs[i][l];
-          while (!Sq0x88Off(x)) {
-            y = Unmap0x88(x);
-            attacks[i][j][k] |= SqBb(y);
-            if ((k << 1) & (1 << (i != 1 ? File(y) : Rank(y))))
-              break;
-            x += dirs[i][l];
-          }
-        }
-      }
+            for (l = 0; l < 2; l++) {
+               x = Map0x88(j) + dirs[i][l];
+               while (!Sq0x88Off(x)) {
+                  y = Unmap0x88(x);
+                  attacks[i][j][k] |= SqBb(y);
+                  if ((k << 1) & (1 << (i != 1 ? File(y) : Rank(y))))
+                     break;
+                  x += dirs[i][l];
+               }
+			}
+         }
 }
 
 void InitPawnAttacks()
 {
-  for (int sq = 0; sq < 64; sq++) {
+   for (int sq = 0; sq < 64; sq++) {
       bbPawnAttacks[WHITE][sq] = GetWPControl(SqBb(sq) );
-         bbPawnAttacks[BLACK][sq] = GetBPControl(SqBb(sq) );
-  }
+      bbPawnAttacks[BLACK][sq] = GetBPControl(SqBb(sq) );
+   }
 }
 
 void InitKnightAttacks()
